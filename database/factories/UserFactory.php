63b5dc49e1716ca3024fb95eb67a702879066bdc
Jannatul Nayeem <123932013+nayeem337@users.noreply.github.com>
2025-08-05 22:44:01 +0600

    using Seeder for Fake Data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name'              => fake()->name(),
+            'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password'          => static::$password ??= Hash::make('password'),
+            'phone'             => fake()->phoneNumber,
+            'address'           => fake()->address,
+            'photo'             => fake()->imageUrl('60', '60'),
+            'role'              => fake()->randomElement(['admin', 'agent', 'user']),
+            'status'            => fake()->randomElement(['active', 'inactive']),
+            'remember_token'    => Str::random(10),
         ];
     }
 
